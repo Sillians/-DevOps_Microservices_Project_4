9206a086948b0810bb1c10713a16bd11c12c5b0f
Sillians <ihuomacbasil@gmail.com>
2021-11-01 04:01:58 +0100

    Modified the f-string in app

---
+++ run_kubernetes.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+TAG=udacity-ml-api
+DOCKERUSER=sillians
+DOCKERPATH=$DOCKERUSER/$TAG
+LOCAL_PORT=8000
+DOCKER_PORT=80
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run $TAG --image=$DOCKERPATH
+sleep 90s  # wait until kubernetes pods
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward deployment/$TAG ${LOCAL_PORT}:${DOCKER_PORT}
